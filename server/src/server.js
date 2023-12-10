12c4417ca2fbce3469cfbf7331c8b35fe553d549
unknown <m.r.fouladi7@gmail.com>
2023-12-10 18:57:40 +0330

    server initialized

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require("./app");
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3500;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening to ${PORT}`);
+});
