56e20f46b56e73e8caba3b0d93132082a9cf005c
unknown <m.r.fouladi7@gmail.com>
2023-12-11 11:46:26 +0330

    jest initialization

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test Get /launches", () => {
+  test("It should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+describe("Test Post /launches", () => {
+  test("It should respond 201 success", () => {});
+
+  test("It should catch missing required properties", () => {});
+  test("It should catch invalid date", () => {});
+});
