098fbf3d4cbd3c272fdef87f3ddaacbac9c53178
unknown <m.r.fouladi7@gmail.com>
2023-12-11 08:03:10 +0330

    package.json updated

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRoute = require("./routes/planets/planets.router");
 
 /**MIDDLEWARES */
-app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(morgan("combined"));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 /** /MIDDLEWARES */
 
 /** ROUTES */
 app.use("/planets", planetsRoute);
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 /** /ROUTES */
 
